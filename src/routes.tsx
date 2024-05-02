225753f9e02dba9afb01d758cdd2dcc5b47f14f6
Ritucreation <sumant.shah@gigacars.com>
2024-05-02 12:45:46 +0530

    modification in routing

--- src/routes.tsx
+++ src/routes.tsx
@@ -4,25 +4,35 @@ import IncDec from "./components/inc_dec";
 import DashboardLayout from "./components/layout/DashboardLayout";
 import Home from "./components/Pages/Home";
 import About from "./components/Pages/About";
+import Login from "./components/layout/Login";
 
 
 const routes: any = [
     {
-        path: '/',
+        path: '/first',
         element: <DashboardLayout />,
         children: [
-            { path: '/inc-dec', element: <IncDec /> },
+            // if we remove slash it makes relative path which is equivalent to  ex : /first/inc-dec
+            { path: 'inc-dec', element: <IncDec /> },
             {
-                path: '/home',
+                path: 'home',
                 element: <Home />
             },
             {
-                path: "/about",
+                path: "about",
                 element: <About />
             }
 
         ]
     },
+
+    {
+        path : '/login',
+        element : <Login/>
+
+    }
+
+
 
 
 ]
